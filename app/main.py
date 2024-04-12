baaea6d8ec5aaf013faaa484ea7c971f1be47be1
zhen <suzan5@naver.com>
2024-04-12 15:57:53 +0900

    230412

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
+from typing import Union
+
 from fastapi import FastAPI
+
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return { "message": "Hello Root!" }
 
-@app.get("/home")
-def home():
-    return { "message": "Home!"}
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
