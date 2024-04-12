ab022fa5a8a518cb983433189c0b85f3fa02d4fd
zhen <suzan5@naver.com>
2024-04-12 17:03:17 +0900

    -m

--- app/main.py
+++ app/main.py
@@ -5,18 +5,23 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    is_offer: Union[bool, None] = None
+name: "sujin"
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    print(name)
+    return 0
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/")
+def create_root():
+    return 0
 
-@app.get("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.put("/")
+def update_root():
+    name = input("type name")
+    return 0
+
+@app.delete("/")
+def delete_root():
+    name = "empty"
+    return 0
